891e6872deb3f451c487d5aec6c9c120aa2d54fb
spconway_umass_edu <spconway_umass_edu@gaoseismolab-cpu001.unity.rc.umass.edu>
2025-03-12 13:29:48 +0000

    getting p rank

---
+++ analysis/bayes_maxdiff_save_p_rank.R
@@ -0,0 +1,27 @@
+# pre-setup ====================================================================================
+rm(list=ls()); gc()
+library(here)
+library(tidyverse)
+library(glue)
+library(HDInterval)
+library(fs)
+library(rstan)
+library(bayesplot)
+
+
+# general setup ====================================================================================
+
+for(which_model in c("maxdiff_1","maxdiff_2","maxdiff_3","maxdiff_separateU_1","maxdiff_separateU_2")){
+  
+  # model settings
+  which_model <- "maxdiff_2"
+  
+  # paths
+  model_dir <- here("analysis","bayes",which_model)
+  
+  load(path(model_dir,glue("{which_model}_fit.RData")))
+  
+  p_rank <- extract(fit,"p_rank")$p_rank
+  save(p_rank,file=path(model_dir,"p_rank.RData"))
+  
+}
