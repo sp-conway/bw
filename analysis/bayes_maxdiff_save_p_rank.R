84d9a8de08e9b7adb036a527f7fad3ea862167d6
spconway_umass_edu <spconway_umass_edu@login1.unity.rc.umass.edu>
2025-03-12 13:32:53 +0000

    fit separate U model with intxns and no main effects, no random effects

--- analysis/bayes_maxdiff_save_p_rank.R
+++ analysis/bayes_maxdiff_save_p_rank.R
@@ -13,14 +13,9 @@ library(bayesplot)
 
 for(which_model in c("maxdiff_1","maxdiff_2","maxdiff_3","maxdiff_separateU_1","maxdiff_separateU_2")){
   
-  # model settings
-  which_model <- "maxdiff_2"
-  
   # paths
   model_dir <- here("analysis","bayes",which_model)
-  
   load(path(model_dir,glue("{which_model}_fit.RData")))
-  
   p_rank <- extract(fit,"p_rank")$p_rank
   save(p_rank,file=path(model_dir,"p_rank.RData"))
   
